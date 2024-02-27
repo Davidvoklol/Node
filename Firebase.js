ecafbc5cf024b0cc6975d157dd0d64d8324292d1
Davidvoklol <polyak.daavid@gmail.com>
2024-02-27 18:46:08 +0100

    Summary

---
+++ Firebase.js
@@ -0,0 +1,37 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getDatabase, ref, get, push, set } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD00nvub_92O_R1uc_JrwThKBH8SIvDCow",
+  authDomain: "first-projekt-7d444.firebaseapp.com",
+  databaseURL: "https://first-projekt-7d444-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "first-projekt-7d444",
+  storageBucket: "first-projekt-7d444.appspot.com",
+  messagingSenderId: "344916382383",
+  appId: "1:344916382383:web:a59363df5a5ec2c8aa3a5a"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getDatabase(app)
+
+const dataRef = ref(db, "My data")
+
+
+function getData() {
+  return get(dataRef)
+    .then(d => d.val())
+    .then(d => d)
+}
+
+function setData(data) {
+  set(dataRef, data)
+    .then(console.log("new data set"))
+}
+
+function addData(data) {
+  push(dataRef, data)
+    .then(console.log("new data added"))
+}
+
+export { app, db, dataRef, getData, setData, addData }
--- app.js
+++ app.js
@@ -1,44 +1,30 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getDatabase, ref, get, push, set } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
+import { app, db, dataRef, getData, setData, addData }
+from "./Firebase.js"
 
-const firebaseConfig = {
-  apiKey: "AIzaSyD00nvub_92O_R1uc_JrwThKBH8SIvDCow",
-  authDomain: "first-projekt-7d444.firebaseapp.com",
-  databaseURL: "https://first-projekt-7d444-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "first-projekt-7d444",
-  storageBucket: "first-projekt-7d444.appspot.com",
-  messagingSenderId: "344916382383",
-  appId: "1:344916382383:web:a59363df5a5ec2c8aa3a5a"
-};
-
-const app = initializeApp(firebaseConfig);
-
-const db = getDatabase(app)
-
-const dataRef = ref(db, "My data")
-
-
-function getData() {
-  get(dataRef)
-    .then(d => console.log(d.val()))
+function getElement(id) {
+  return document.getElementById(id)
 }
 
-function setData(data) {
-  set(dataRef, data)
-    .then(console.log("new data set"))
+const dataBG = getElement("showdata")
+const dataBH = getElement("hidedata")
+const dataP = getElement("data")
+const dataBS = getElement("setdata")
+const dataBA = getElement("adddata")
+const input = getElement("input")
+
+async function showData() {
+  const content = await getData()
+  dataP.innerHTML = content
+  console.log(content)
+}
+function hideData() {
+  dataP.innerHTML = null
+}
+function SetData() {
+  setData(input.value)
+  input.value = null
 }
 
-function addData(data) {
-  push(dataRef, data)
-    .then(console.log("new data added"))
-}
-
-
-
-
-setData(null)
-
-addData("i set this data in the database")
-addData("another data to add to the database")
-
-getData()
+dataBG.addEventListener("click", showData)
+dataBH.addEventListener("click", hideData)
+dataBS.addEventListener("click", SetData)
